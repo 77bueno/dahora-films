2c3fe7dad9dfb71478798f147d22bc44e8615cd6
victor <victorhugomotabueno@gmail.com>
2024-02-26 09:16:31 -0300

    Comentários e usando o NavigationContainer

--- App.js
+++ App.js
@@ -1,15 +1,23 @@
 import { StatusBar } from "react-native";
+import { NavigationContainer } from "@react-navigation/native"; 
+import { createNativeStackNavigator } from "@react-navigation/native-stack"; 
 import Home from "./src/screens/Home";
 import Sobre from "./src/screens/Sobre";
 import Privacidade from "./src/screens/Privacidade";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <>
       <StatusBar barStyle="light-content" />
-      {/* <Home /> */}
-      {/* <Sobre /> */}
-      <Privacidade />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="Privacidade" component={Privacidade} />
+          <Stack.Screen name="Sobre" component={Sobre} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
   );
 }
