c03035c5b3b0dadcde6c6d823186d5872371c54b
victor <victorhugomotabueno@gmail.com>
2024-03-11 09:44:27 -0300

    Estilização do SafeContainer

--- src/screens/Detalhes.js
+++ src/screens/Detalhes.js
@@ -1,11 +1,55 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { ImageBackground, ScrollView, StyleSheet, Text, View } from 'react-native';
+import SafeContainer from '../components/SafeContainer';
+import ImagemAlternativa from '../../assets/images/foto-alternativa.jpg'
 
 export default function Detalhes() {
   return (
-    <View>
-      <Text>Detalhes</Text>
-    </View>
+    <SafeContainer>
+      <View style={estilos.subContainer}>
+        <ImageBackground style={estilos.imagemFundo} source={ImagemAlternativa}>
+          <Text style={estilos.tituloFilme}>Título do filme...</Text>
+        </ImageBackground>
+
+        <View style={estilos.conteudo}>
+          <ScrollView>
+            <Text style={[estilos.texto, estilos.avaliacao]}>Avaliação...</Text>
+            <Text style={[estilos.texto, estilos.lancamento]}>Lançamento...</Text>
+            <Text style={[estilos.texto, estilos.descricao]}>Descrição...</Text>
+          </ScrollView>
+        </View>
+      </View>
+    </SafeContainer>
   )
 }
 
-const styles = StyleSheet.create({})
+const estilos = StyleSheet.create({
+  subContainer: {
+    flex: 1,
+    width: "100%",
+  },
+  imagemFundo: {
+    height: 200,
+    justifyContent: "center"
+  },
+  tituloFilme: {
+    backgroundColor: "rgba(0,0,0,0.55)",
+    color: "white",
+    fontFamily: "NotoSans",
+    textAlign: "center",
+    padding: 16,
+    fontSize: 16,
+    fontWeight: "bold"
+  },
+  conteudo: {
+    padding: 16,
+    flex: 1 /* Apenas para garantir a ocupação do
+    espaço em caso de texto muito grande*/
+  },
+  texto: {
+    paddingVertical: 4,
+    fontSize: 16 
+  },
+  avaliacao: { color: "blue" },
+  lancamento: { color: "darkblue" },
+  descricao: { color: "gray" },
+})
